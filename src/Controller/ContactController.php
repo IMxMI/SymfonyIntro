466385dedf135a688ff55278eb6f3d182313fce7
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-04 10:49:06 +0100

    controlleur demande contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Contact;
 
-class ContactController extends AbstractController
-{
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
-    {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+class ContactController extends AbstractController {
+
+    #[Route('/demande', name: 'demande')]
+    public function demandeContact(Request $request, GestionContact $gestionContact): Response {
+        $contact = new Contact();
+        $form = $this->createFormBuilder($contact)
+                ->add('titre')
+                ->add('nom')
+                ->add('prenom')
+                ->add('mail')
+                ->add('tel')
+                ->getForm();
+        return $this->render('contact/contact.html.twig', [
+                    'formContact' => $form->createView(),
+                    'titre' => 'Formulaire de conttact',
         ]);
     }
 }
