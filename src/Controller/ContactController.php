d143a85c9f260a903a417ce4dc3bc0d766229931
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-04 11:04:12 +0100

    Parametre formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,18 +7,43 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Contact;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-#[Route('/contact', name:'contact_')]
+#[Route('/contact', name: 'contact_')]
 class ContactController extends AbstractController {
 
     #[Route('/demande', name: 'demande')]
     public function demandeContact(Request $request): Response {
         $contact = new Contact();
         $form = $this->createFormBuilder($contact)
-                ->add('titre')
-                ->add('nom')
-                ->add('mail')
-                ->add('tel')
+                ->add('titre', ChoiceType::class, array(
+                    'choices' => array(
+                        'Monsieur' => 'M',
+                        'Madame' => 'F',
+                    ), 'multiple' => false,
+                    'expanded' => true,
+                ))
+                ->add('nom', TextType::class,
+                        array(
+                            'label' => 'Nom : ',
+                            'required' => true,
+                            'attr' => ['placeholder' => 'votre nom'],
+                        ))
+                ->add('mail', EmailType::class,
+                        array(
+                            'label' => 'Mail : ',
+                            'required' => true,
+                        ))
+                ->add('tel', TelType::class,
+                        array(
+                            'label' => 'Téléphone : ',
+                            'required' => true,
+                        ))
+                ->add('Envoyer', SubmitType::class)
                 ->getForm();
         return $this->render('contact/contact.html.twig', [
                     'formContact' => $form->createView(),
