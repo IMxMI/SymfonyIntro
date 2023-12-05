6ce92d9dd22da5c19894d22eeee23d8c2fc2a82d
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-05 08:49:06 +0100

    Update GestionContact.php

--- src/Service/GestionContact.php
+++ src/Service/GestionContact.php
@@ -1,21 +1,40 @@
 <?php
+
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Contact;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
-class GestionContact{
-    
+class GestionContact {
+
     private EntityManagerInterface $em;
-    
-    public function __construct(EntityManagerInterface $em){
+    private MailerInterface $mailer;
+
+    public function __construct(EntityManagerInterface $em, MailerInterface $mailer) {
         $this->em = $em;
+        $this->mailer = $mailer;
     }
-    
-    public function creerContact(Contact $contact){
+
+    public function creerContact(Contact $contact) {
         $contact->setDatePremierContact(new \DateTime());
         $this->em->persist($contact);
         $this->em->flush();
     }
-}
 
+    public function envoieMailContact(Contact $contact) {
+        $email = (new TemplatedEmail())
+                ->from(new Address('mxm.vernoux@gmail.com', 'Contact Symfony'))
+                ->to($contact->getMail())
+                ->subject('Demande de renseignement')
+                ->text('Bonjour')
+                ->attachFromPath('assets/pdf/presentation.pdf', 'Présentation')
+                ->htmlTemplate('mails/mail.html.twig')
+                ->context([
+            'contact' => $contact
+        ]);
+        $this->mailer->send($email);
+    }
+}
