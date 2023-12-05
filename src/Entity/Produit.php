4b9e931794c2d49c9edaadfac9a29526a1e3f78c
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-12-05 08:48:56 +0100

    migration

---
+++ src/Entity/Produit.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column]
+    private ?float $puht = null;
+
+    #[ORM\Column]
+    private ?float $remise = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPuht(): ?float
+    {
+        return $this->puht;
+    }
+
+    public function setPuht(float $puht): static
+    {
+        $this->puht = $puht;
+
+        return $this;
+    }
+
+    public function getRemise(): ?float
+    {
+        return $this->remise;
+    }
+
+    public function setRemise(float $remise): static
+    {
+        $this->remise = $remise;
+
+        return $this;
+    }
+}
